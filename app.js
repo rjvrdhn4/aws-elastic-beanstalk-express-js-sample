add07246fa1ed60f26430168860f9f27f4d3cef7
rjvrdhn4 <99847443+rjvrdhn4@users.noreply.github.com>
2022-04-13 20:06:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Project Pipeline created'));
+app.get('/', (req, res) => res.send('Project git'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
